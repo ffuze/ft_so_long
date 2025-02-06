ee9dc0c5e180dd00dbd199a7dac62019dacca233
Alex Degl'Innocenti <adegl-in@c2r5p4.42firenze.it>
2025-02-06 20:09:54 +0100

    edited the game structures and added libft.h

--- mandatory/index.h
+++ mandatory/index.h
@@ -4,25 +4,54 @@
 #include <X11/X.h>
 #include <X11/keysym.h>
 
-typedef struct s_data
+# define TILE_SIZE 300
+
+// manages minilibx initialization and the window
+typedef struct s_window
 {
 	void		*mlx_ptr; // MLX pointer
 	void		*win_ptr; // MLX window pointer
-}	t_data;
+	t_textures		*img_ptr; // Image pointer
+}	t_window;
 
+// manages and holds all the images used for the textures
+typedef struct s_textures {
+	void	*floor; // "empty space"
+    void	*wall;
+    void	*collectible;
+    void	*exit;
+    void	*player;
+	void	*enemy;
+} t_textures;
 
-int on_destroy(t_data *data)
+// all the info about the map, such as its width, height and the player position
+typedef struct s_map {
+    char	**grid;
+    int		width;
+    int		height;
+    int		x;
+    int		y;
+}	t_map;
+
+// a strucutre that connects all previous ones
+typedef struct s_game {
+    t_window	window;
+    t_map		map;
+    t_textures	textures;
+}	t_game;
+
+int on_destroy(t_window *window)
 {
-	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-	mlx_destroy_display(data->mlx_ptr);
-	free(data->mlx_ptr);
+	mlx_destroy_window(window->mlx_ptr, window->win_ptr);
+	mlx_destroy_display(window->mlx_ptr);
+	free(window->mlx_ptr);
 	exit(0);
 	return (0);
 }
 
-int on_keypress(int keysym, t_data *data)
+int on_keypress(int keysym, t_window *window)
 {
-	(void)data;
+	(void)window;
 	printf("Pressed key: %d\\n", keysym);
 	return (0);
 }
