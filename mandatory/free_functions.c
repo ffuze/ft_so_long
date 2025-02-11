71feff568f1ecca69326b8b43226b0bd78c253fd
Alex Degl'Innocenti <adegl-in@c2r2p4.42firenze.it>
2025-02-11 19:37:54 +0100

    added and improved memory allocation functions

--- mandatory/free_functions.c
+++ mandatory/free_functions.c
@@ -6,21 +6,44 @@
 /*   By: adegl-in <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 16:44:03 by adegl-in          #+#    #+#             */
-/*   Updated: 2025/02/10 16:50:14 by adegl-in         ###   ########.fr       */
+/*   Updated: 2025/02/11 16:20:50 by adegl-in         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "index.h"
-#include <stdlib.h>
 
-void	free_grid(t_game *game)
+void	ft_free_grid(char **grid)
 {
-	int y;
-	
-	while (game->map.grid[y])
+	int	i;
+
+	i = 0;
+	while (grid[i])
 	{
-		free(game->map.grid[y]);
-		y++;
+		free(grid[i]);
+		i++;
 	}
-	free(game->map.grid);
+	free (grid);
+}
+
+void	free_textures(t_game *game)
+{
+	mlx_destroy_image(game->window.mlx_ptr, game->textures.collectible);
+	mlx_destroy_image(game->window.mlx_ptr, game->textures.enemy);
+	mlx_destroy_image(game->window.mlx_ptr, game->textures.exit);
+	mlx_destroy_image(game->window.mlx_ptr, game->textures.floor);
+	mlx_destroy_image(game->window.mlx_ptr, game->textures.player);
+	mlx_destroy_image(game->window.mlx_ptr, game->textures.wall);
+}
+
+void	free_everything(t_game *game)
+{
+	ft_free_maps(game->map);
+	free_textures(game);
+	if (game->window.win_ptr)
+		mlx_destroy_window(game->window.mlx_ptr, game->window.win_ptr);
+	if (game->window.mlx_ptr)
+	{
+		mlx_destroy_display(game->window.mlx_ptr);
+		free(game->window.mlx_ptr);
+	}
 }
