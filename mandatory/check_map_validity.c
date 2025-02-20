bae737520490922e95e5d66aec4e52dc43130afc
Alex Degl'Innocenti <adegl-in@c2r2p4.42firenze.it>
2025-02-20 18:13:56 +0100

    added map validity controls + security push

--- mandatory/check_map_validity.c
+++ mandatory/check_map_validity.c
@@ -6,137 +6,153 @@
 /*   By: adegl-in <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 16:51:18 by adegl-in          #+#    #+#             */
-/*   Updated: 2025/02/18 17:49:57 by adegl-in         ###   ########.fr       */
+/*   Updated: 2025/02/20 18:12:14 by adegl-in         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "index.h"
 
 // flood fill to check map validity
-void flood_fill(char **grid, int x, int y, int height, int width, char target, char replace)
+
+// util
+static int	ft_strlen_no_newline(char *str)
 {
-    // check bounds and stop if out of bounds or not the target character
-    if (x < 0 || x >= width || y < 0 || y >= height || grid[y][x] != target)
-        return;
+	int len = 0;
 
-    // replace current position with a visited marker
-    grid[y][x] = replace;
-
-    flood_fill(grid, x + 1, y, height, width, target, replace); // right
-    flood_fill(grid, x - 1, y, height, width, target, replace); // left
-    flood_fill(grid, x, y + 1, height, width, target, replace); // down
-    flood_fill(grid, x, y - 1, height, width, target, replace); // up
+	while (str[len] && str[len] != '\n')
+		len++;
+	return (len);
 }
-
-int is_map_valid(char **grid, int height, int width)
+static int	is_symmetric(char **map, int height)
 {
-    int player_x = 0, player_y = 0;
-    int exit_x = 0, exit_y = 0;
-    int i = 0, j = 0;
-	
-    while (i < height)
-	{
-        j = 0;
-        while (j < width)
-		{
-            if (grid[i][j] == 'P')
-			{
-                player_x = j;
-                player_y = i;
-            }
-            if (grid[i][j] == 'E')
-			{
-                exit_x = j;
-                exit_y = i;
-            }
-            j++;
-        }
-        i++;
-    }
+	int i, width, widthf;
 
-    // start flood fill starting from the player
-    flood_fill(grid, player_x, player_y, height, width, '0', 'G');  // 'G' marks reachable areas
-
-    // check if the exit and all collectibles are reachable
-    if (grid[exit_y][exit_x] != 'G')
-	{
-        printf("Exit is unreachable\n");
-        return 0;
-    }
-
-    // check if all collectibles are reachable
-    i = 0;
-    while (i < height)
-	{
-        j = 0;
-        while (j < width)
-		{
-            if (grid[i][j] == 'C' && grid[i][j] != 'G')
-			{
-                printf("Collectible at (%d, %d) is unreachable\n", j, i);
-                return 0;
-            }
-            j++;
-        }
-        i++;
-    }
-
-    return 1;
-}
-
-int	check_wall(t_game *game)
-{
-	int x;
-	int y;
-	int i;
-
-	x = game->map.width;
-	y = game->map.height;
+	if (!map || !map[0])
+		return (0);
+	widthf = ft_strlen_no_newline(map[0]);
 	i = 0;
-	while (i < x)
+	while (i < height)
 	{
-		if (game->map.grid[0][i] != '1' || game->map.grid[y - 1][i] != '1')
-			return (0);
-		i++;
-	}
-	i = 0;
-	while (i < y)
-	{
-		if (game->map.grid[i][0] != '1' || game->map.grid[i][x - 1] != '1')
+		width = (int)ft_strlen_no_newline(map[i]);
+		if (width != widthf)
 			return (0);
 		i++;
 	}
 	return (1);
 }
 
-int	check_char (t_game *game)
+int	check_walls(char **map, int height, int width)
 {
 	int i;
-	int j;
-	int p;
-	int e;
-	int c;
+
+	if (!is_symmetric(map, height))
+		return (0);
+	i = -1;
+	while (++i < width)
+		if (map[0][i] != '1' || map[height - 1][i] != '1')
+			return (0);
+	i = -1;
+	while (++i < height)
+		if (map[i][0] != '1' || map[i][width - 1] != '1')
+			return (0);
+	return (1);
+}
+
+void	find_player(t_game *game)
+{
+	int y, x;
+	y = -1;
+	game->calcs.total_score = 0;
+	while (++y < game->map.height)
+	{
+		x = -1;
+		while (++x < game->map.width)
+		{
+			if (game->map.grid[y][x] == 'P')
+			{
+				game->map.player_x = x;
+				game->map.player_y = y;
+			}
+			else if (game->map.grid[y][x] == 'C')
+				game->calcs.total_score++;
+		}
+	}
+}
+
+int	validate_elements(int p, int c, int e)
+{
+	return (p == 1 && e == 1 && c >= 1);
+}
+
+int	count_characters(char *str, int *p, int *c, int *e)
+{
+	int	i;
 
 	i = 0;
-	p = 0;
-	e = 0;
-	c = 0;
-	while (i < game->map.height)
+	while (str[i])
 	{
-		j = 0;
-		while (j < game->map.width)
+		ft_printf("COUNT CHARACTERS p: %d, c: %d, e: %d, i: %d\n", *p, *c, *e, i);
+		ft_printf("%s", str);
+		if (str[i] == '1')
 		{
-			if (game->map.grid[i][j] == 'P')
-				p++;
-			if (game->map.grid[i][j] == 'E')
-				e++;
-			if (game->map.grid[i][j] == 'C')
-				c++;
-			j++;
+			i++;
+			continue;
 		}
+		else if (str[i] == 'P')
+			(*p)++;
+		else if (str[i] == 'C')
+			(*c)++;
+		else if (str[i] == 'E')
+			(*e)++;
+		if (str[i] != 'C' && str[i] != 'P' && str[i] != 'E'
+			&& str[i] != '1' && str[i] != '0' && str[i] != '\n')
+			return (0);
 		i++;
 	}
-	if (p != 1 || e != 1 || c < 1)
-		return (0);
+	return (1);
+}
+
+void	flood_fill(char **map, int x, int y, int height, int width, int *reachable_c, int *reachable_e)
+{
+	if (x < 0 || x >= width || y < 0 || y >= height || map[y][x] == '1' || map[y][x] == 'F')
+		return ;
+	if (map[y][x] == 'C')
+		(*reachable_c)++;
+	else if (map[y][x] == 'E')
+		(*reachable_e)++;
+
+	flood_fill(map, x + 1, y, height, width, reachable_c, reachable_e);
+	flood_fill(map, x - 1, y, height, width, reachable_c, reachable_e);
+	flood_fill(map, x, y + 1, height, width, reachable_c, reachable_e);
+	flood_fill(map, x, y - 1, height, width, reachable_c, reachable_e);
+}
+
+int	is_map_valid(t_game *game)
+{
+	int p;
+	int c;
+	int e;
+	int i;
+	char **map_copy;
+
+	p = 0;
+	c = 0;
+	e = 0;
+	i = -1;
+	while (++i < game->map.height)
+	{
+		if (!count_characters(game->map.grid[i], &p, &c, &e))
+			return (0);
+		// if (i == 0)
+		// 	continue;
+		if (!check_walls(game->map.grid, game->map.height, game->map.width) && !validate_elements(p, c, e))
+			return (0);
+	}
+	map_copy = malloc(game->map.height * sizeof(char *));
+	i = 0;
+	while (i < game->map.height)
+		map_copy[i++] = ft_strdup(game->map.grid[i]);
+	find_player(game);
+	// flood_fill(map_copy, game->map.player_x, game->map.player_y, );
 	return (1);
 }
